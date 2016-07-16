0b6e714e79f73671e243605154c3e69a9bb1efce
Valter Correia <valtergcorreia@ua.pt>
2016-07-16 16:48:51 +0100

    Simple working version

--- csgopricer/pricer.js
+++ csgopricer/pricer.js
@@ -1,6 +1,8 @@
-document.body.style.border = "5px solid green";
+//document.body.style.border = "5px solid green";
 
 //Retrieves the inventory for the user
+
+var x = document.getElementsByClassName("itemHolder");
 
 var userid = document.URL.split("/")[4];
 
@@ -12,16 +14,21 @@ xreq.send();
 //Parses the inventory json
 
 if(xreq.status == 200){
+	var slot_idx = 0
+
 	var json_invetory = JSON.parse(xreq.responseText);
 
-	var inv_info = json_invetory.rgDescriptions;
+	var rgInventory = json_invetory.rgInventory;
 
-	for(var item in inv_info){
+	var rgDescriptions = json_invetory.rgDescriptions;
 
-		var item_market_hash = inv_info[item].market_hash_name;
+	for(var item in rgInventory){
 
-		console.log(item_market_hash);
+		var description_id = rgInventory[item].classid + "_" + rgInventory[item].instanceid;
 
+		var item_market_hash = rgDescriptions[description_id].market_hash_name;
+
+		// Returns item price from market
 		xreq.open("GET", "http://steamcommunity.com/market/priceoverview/?currency=1&appid=730&market_hash_name=" + item_market_hash, false);
 		xreq.send();
 
@@ -30,10 +37,28 @@ if(xreq.status == 200){
 
 			var item_price = json_prices.lowest_price;
 
-			console.log(item_price);
+			slot_idx++;
+
+			//Creates a new div that encapsulates the price
+
+			var price_elem = document.createElement("div")
+			price_elem.style.position = "absolute";
+			price_elem.style.bottom = "15px";
+			price_elem.style.height = "14px";
+			price_elem.style.width = "100%";
+			price_elem.style.backgroundColor = "#ff0000";
+			price_elem.style.color = "#ffffff";
+			var price_text = document.createTextNode(item_price);
+
+			price_elem.appendChild(price_text);
+
+			var tmp = document.getElementById("item730_2_" + rgInventory[item].id)
+
+			//Adds the price to the page
+			tmp.insertBefore(price_elem, tmp[1]);
+
 		}
-
 	}
-}
 
-//console.log(json);
+	
+}
